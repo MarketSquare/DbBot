386d08c373a1debb370966a6cc800e1c0b3e2c09
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-04-23 13:48:47 +0300

    An example for outputting html test run summaries

--- examples/topfail/topfail/database_reader.py
+++ examples/topfail/topfail/database_reader.py
@@ -5,17 +5,37 @@ from dbbot import RobotDatabase
 
 class DatabaseReader(RobotDatabase):
 
-    def __init__(self, db_file_path, output):
-        super(DatabaseReader, self).__init__(db_file_path, output)
+    def __init__(self, db_file_path):
+        super(DatabaseReader, self).__init__(db_file_path, None)
         self._connection.row_factory = sqlite3.Row
 
-    def failed_suites(self):
+    def most_failed_suites(self):
         sql_statement = '''
             SELECT count() as count, suites.id, suites.name, suites.source
             FROM suites, suite_status
             WHERE suites.id == suite_status.suite_id AND
             suite_status.status == "FAIL"
             GROUP BY suites.source
+        '''
+        return self._connection.execute(sql_statement).fetchall()
+
+    def most_failed_tests(self):
+        sql_statement = '''
+            SELECT count() as count, tests.id, tests.name, tests.suite_id
+            FROM tests, test_status
+            WHERE tests.id == test_status.test_id AND
+            test_status.status == "FAIL"
+            GROUP BY tests.name
+        '''
+        return self._connection.execute(sql_statement).fetchall()
+
+    def most_failed_keywords(self):
+        sql_statement = '''
+            SELECT count() as count, keywords.name, keywords.type
+            FROM keywords, keyword_status
+            WHERE keywords.id == keyword_status.keyword_id AND
+            keyword_status.status == "FAIL"
+            GROUP BY keywords.name, keywords.type
         '''
         return self._connection.execute(sql_statement).fetchall()
 
--- examples/topfail/topfail/html_writer.py
+++ examples/topfail/topfail/html_writer.py
@@ -1,16 +1,51 @@
+from string import Template
+
 from dbbot import Logger
 
 
+TEMPLATE_PATH = '../data'
+
 class HtmlWriter(object):
 
-    def __init__(self, db, output):
-        self._verbose = Logger('HtmlWriter', output)
+    def __init__(self, db, output_file_path):
         self._db = db
+        self._output_file_path = output_file_path
+        self._load_layouts()
+
+    def _load_layouts(self):
+        self._full_layout = Template(open('%s/layout.html' % TEMPLATE_PATH, 'r').read())
+        self._table_layout = Template(open('%s/table.html' % TEMPLATE_PATH, 'r').read())
+        self._row_layout = Template(open('%s/row.html' % TEMPLATE_PATH, 'r').read())
 
     def write(self):
-        for suite in self._db.failed_suites():
-            print "SUITE: %i times: %s (%s)" % (suite['count'], suite['name'], suite['source'])
-            for test in self._db.failed_tests_for_suite(suite['id']):
-                print " `--> TEST: %i times: %s" % (test['count'], test['name'])
-                for keyword in self._db.failed_keywords_for_test(test['id']):
-                    print "   `--> KEYWORD: %i times: %s" % (keyword['count'], keyword['name'])
+        output = self._full_layout.substitute({
+            'most_failed_suites': self._most_failed_suites(),
+            'most_failed_tests': self._most_failed_tests(),
+            'most_failed_keywords': self._most_failed_keywords()
+        })
+        with open(self._output_file_path, 'w') as file:
+            file.write(output)
+
+    def _most_failed_suites(self):
+        rows = [self._format_row(suite) for suite in self._db.most_failed_suites()]
+        return self._format_table('most failed suites', rows)
+
+    def _most_failed_tests(self):
+        rows = [self._format_row(test) for test in self._db.most_failed_tests()]
+        return self._format_table('most failed tests', rows)
+
+    def _most_failed_keywords(self):
+        rows = [self._format_row(keyword) for keyword in self._db.most_failed_keywords()]
+        return self._format_table('most failed keywords', rows)
+
+    def _format_table(self, title, rows):
+        return self._table_layout.substitute({
+            'table_title': title,
+            'rows': ''.join(rows)
+        })
+
+    def _format_row(self, item):
+        return self._row_layout.substitute({
+            'name': item['name'],
+            'count': item['count']
+        })
