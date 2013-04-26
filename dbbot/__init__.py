39b0d9e5b7fbd40cdf8c1fd3c5fdf36e06df02f6
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-04-26 16:05:18 +0300

    Moved non-absract DbBot classes to 'reader' package

--- dbbot/__init__.py
+++ dbbot/__init__.py
@@ -1,6 +1,3 @@
 from .command_line_options import CommandLineOptions
 from .logger import Logger
 from .robot_database import RobotDatabase
-from .database_writer import DatabaseWriter
-from .reader_options import ReaderOptions
-from .results_parser import ResultsParser
---
+++ dbbot/reader/__init__.py
@@ -0,0 +1,3 @@
+from .database_writer import DatabaseWriter
+from .reader_options import ReaderOptions
+from .results_parser import ResultsParser
