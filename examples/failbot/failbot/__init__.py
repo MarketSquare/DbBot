264e9025f42563ae028caba5e5f678cfa3f3e667
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-04-26 11:49:09 +0300

    Try appending DbBot root by default

--- examples/failbot/failbot/__init__.py
+++ examples/failbot/failbot/__init__.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+# default dbbot root
+sys.path.append(os.path.abspath(__file__ + '/../../../../'))
+
 from .database_reader import DatabaseReader
 from .writer_options import WriterOptions
 from .html_writer import HtmlWriter
