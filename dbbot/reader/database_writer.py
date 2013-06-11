5048084e128bc41851717a5d72c86707b02bcadb
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-06-11 17:35:51 +0300

    Robot Framework 2.8 compatibility (database migrator included)

--- dbbot/reader/database_writer.py
+++ dbbot/reader/database_writer.py
@@ -11,13 +11,29 @@ class DatabaseWriter(RobotDatabase):
 
     def _init_schema(self):
         self._verbose('- Initializing database schema')
+        self._create_table_test_runs()
+        self._create_table_test_run_status()
+        self._create_table_test_run_errors()
+        self._create_table_tag_status()
+        self._create_table_suites()
+        self._create_table_suite_status()
+        self._create_table_tests()
+        self._create_table_test_status()
+        self._create_table_keywords()
+        self._create_table_keyword_status()
+        self._create_table_messages()
+        self._create_table_tags()
+        self._create_table_arguments()
+
+    def _create_table_test_runs(self):
         self._create_table('test_runs', {
             'source_file': 'TEXT NOT NULL',
-            'generator': 'TEXT NOT NULL',
             'started_at': 'DATETIME NOT NULL',
             'finished_at': 'DATETIME NOT NULL',
             'imported_at': 'DATETIME NOT NULL',
-        }, ('generator', 'source_file', 'started_at', 'finished_at'))
+        }, ('source_file', 'started_at', 'finished_at'))
+
+    def _create_table_test_run_status(self):
         self._create_table('test_run_status', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'name': 'TEXT NOT NULL',
@@ -25,12 +41,16 @@ class DatabaseWriter(RobotDatabase):
             'failed': 'INTEGER NOT NULL',
             'passed': 'INTEGER NOT NULL'
         }, ('test_run_id', 'name'))
+
+    def _create_table_test_run_errors(self):
         self._create_table('test_run_errors', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'level': 'TEXT NOT NULL',
             'timestamp': 'DATETIME NOT NULL',
             'content': 'TEXT NOT NULL'
         }, ('test_run_id', 'level', 'content'))
+
+    def _create_table_tag_status(self):
         self._create_table('tag_status', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'name': 'TEXT NOT NULL',
@@ -39,6 +59,8 @@ class DatabaseWriter(RobotDatabase):
             'failed': 'INTEGER NOT NULL',
             'passed': 'INTEGER NOT NULL',
         }, ('test_run_id', 'name'))
+
+    def _create_table_suites(self):
         self._create_table('suites', {
             'suite_id': 'INTEGER REFERENCES suites',
             'xml_id': 'TEXT NOT NULL',
@@ -46,6 +68,8 @@ class DatabaseWriter(RobotDatabase):
             'source': 'TEXT NOT NULL',
             'doc': 'TEXT NOT NULL'
         }, ('name', 'source'))
+
+    def _create_table_suite_status(self):
         self._create_table('suite_status', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'suite_id': 'INTEGER  NOT NULL REFERENCES suites',
@@ -54,44 +78,58 @@ class DatabaseWriter(RobotDatabase):
             'passed': 'INTEGER NOT NULL',
             'status': 'TEXT NOT NULL'
         }, ('test_run_id', 'suite_id'))
+
+    def _create_table_tests(self):
         self._create_table('tests', {
             'suite_id': 'INTEGER NOT NULL REFERENCES suites',
             'xml_id': 'TEXT NOT NULL',
             'name': 'TEXT NOT NULL',
-            'timeout': 'TEXT NOT NULL',
+            'timeout': 'TEXT',
             'doc': 'TEXT NOT NULL'
         }, ('suite_id', 'name'))
+
+    def _create_table_test_status(self):
         self._create_table('test_status', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'test_id': 'INTEGER  NOT NULL REFERENCES tests',
             'status': 'TEXT NOT NULL',
             'elapsed': 'INTEGER NOT NULL'
         }, ('test_run_id', 'test_id'))
+
+    def _create_table_keywords(self):
         self._create_table('keywords', {
             'suite_id': 'INTEGER REFERENCES suites',
             'test_id': 'INTEGER REFERENCES tests',
             'keyword_id': 'INTEGER REFERENCES keywords',
             'name': 'TEXT NOT NULL',
             'type': 'TEXT NOT NULL',
-            'timeout': 'TEXT NOT NULL',
+            'timeout': 'TEXT',
             'doc': 'TEXT NOT NULL'
         }, ('name', 'type'))
+
+    def _create_table_keyword_status(self):
         self._create_table('keyword_status', {
             'test_run_id': 'INTEGER NOT NULL REFERENCES test_runs',
             'keyword_id': 'INTEGER NOT NULL REFERENCES keyword',
             'status': 'TEXT NOT NULL',
             'elapsed': 'INTEGER NOT NULL'
         }, ('test_run_id', 'keyword_id'))
+
+    def _create_table_messages(self):
         self._create_table('messages', {
             'keyword_id': 'INTEGER NOT NULL REFERENCES keywords',
             'level': 'TEXT NOT NULL',
             'timestamp': 'DATETIME NOT NULL',
             'content': 'TEXT NOT NULL'
         }, ('keyword_id', 'level', 'content'))
+
+    def _create_table_tags(self):
         self._create_table('tags', {
             'test_id': 'INTEGER NOT NULL REFERENCES tests',
             'content': 'TEXT NOT NULL'
         }, ('test_id', 'content'))
+
+    def _create_table_arguments(self):
         self._create_table('arguments', {
             'keyword_id': 'INTEGER NOT NULL REFERENCES keywords',
             'content': 'TEXT NOT NULL'
@@ -107,6 +145,24 @@ class DatabaseWriter(RobotDatabase):
                 table_name, unique_column_names)
             )
         sql_statement = 'CREATE TABLE IF NOT EXISTS %s (%s)' % (table_name, ', '.join(definitions))
+        self._connection.execute(sql_statement)
+
+    def rename_table(self, old_name, new_name):
+        sql_statement = 'ALTER TABLE %s RENAME TO %s' % (old_name, new_name)
+        self._connection.execute(sql_statement)
+
+    def drop_table(self, table_name):
+        sql_statement = 'DROP TABLE %s' % table_name
+        self._connection.execute(sql_statement)
+
+    def copy_table(self, from_table, to_table, columns_to_copy):
+        column_names = ', '.join(columns_to_copy)
+        sql_statement = 'INSERT INTO %s(%s) SELECT %s FROM %s' % (
+            to_table,
+            column_names,
+            column_names,
+            from_table
+        )
         self._connection.execute(sql_statement)
 
     def fetch_id(self, table_name, criteria):
