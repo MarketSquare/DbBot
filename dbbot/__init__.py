fea222aa8a72820d64cd47b62708ba765ee66185
Tatu Kairi <tatu.kairi@eficode.com>
2014-01-30 18:57:02 +0200

    Updates #8:
    Removes --files option. Combines dbbot/command_line_options.py to
    dbbot/reader/reader_options.py.
    
    Enhances tests relating to command line arguments
    
    Documentation still needs to be done.

--- dbbot/__init__.py
+++ dbbot/__init__.py
@@ -1,7 +1,6 @@
 
 __version__ = '0.1-devel'
 
-from .command_line_options import CommandLineOptions
 from .logger import Logger
 from .robot_database import RobotDatabase
 
--- dbbot/reader/reader_options.py
+++ dbbot/reader/reader_options.py
@@ -1,14 +1,67 @@
+from optparse import OptionParser
 from os.path import exists
-from sys import argv
-
-from dbbot import CommandLineOptions
 
 
-class ReaderOptions(CommandLineOptions):
+DEFAULT_DB_NAME = 'robot_results.db'
+
+class ReaderOptions(object):
+
+    def __init__(self):
+        self._parser = OptionParser()
+        self._add_parser_options()
+        self._options, self._files = self._get_validated_options()
+
+    def _add_parser_options(self):
+        options = [
+            ('-d', '--dry-run', {'action': 'store_true',
+                                 'default': False,
+                                 'dest': 'dry_run',
+                                 'help': 'do everything except store results into disk'}),
+
+            ('-k', '--also-keywords', {'action':'store_true',
+                                       'default': False,
+                                       'dest': 'include_keywords',
+                                       'help': 'parse also suites\' and tests\' keywords'}),
+
+            ('-v', '--verbose', {'action': 'store_true',
+                                 'default': False,
+                                 'dest': 'be_verbose',
+                                 'help': 'be verbose about the operation'}),
+
+            ('-b', '--database', {'dest': 'db_file_path',
+                                  'default': DEFAULT_DB_NAME,
+                                  'help': 'path to the SQLite database for test run results'})
+        ]
+        for option in options:
+            self._parser.add_option(option[0], option[1], **option[2])
+
+    def _get_validated_options(self):
+        options, files = self._parser.parse_args()
+        self._check_files(files)
+        return options, files
+
+    def _check_files(self, files):
+        if not files or len(files) < 1:
+            self._parser.error('at least one input file is required')
+        for file_path in files:
+            if not exists(file_path):
+                self._parser.error('file "%s" does not exist' % file_path)
+
+    def _exit_with_help(self):
+        self._parser.print_help()
+        exit(1)
+
+    @property
+    def db_file_path(self):
+        return self._options.db_file_path
+
+    @property
+    def be_verbose(self):
+        return self._options.be_verbose
 
     @property
     def file_paths(self):
-        return self._options.file_paths
+        return self._files
 
     @property
     def dry_run(self):
@@ -17,44 +70,3 @@ class ReaderOptions(CommandLineOptions):
     @property
     def include_keywords(self):
         return self._options.include_keywords
-
-    def _add_parser_options(self):
-        super(ReaderOptions, self)._add_parser_options()
-        def files_args_parser(option, opt_str, _, parser):
-            values = []
-            for arg in parser.rargs:
-                if arg[:2] == '--' and len(arg) > 2:
-                    break
-                if arg[:1] == '-' and len(arg) > 1:
-                    break
-                values.append(arg)
-            del parser.rargs[:len(values)]
-            setattr(parser.values, option.dest, values)
-
-        self._parser.add_option('-d', '--dry-run',
-            action='store_true',
-            dest='dry_run',
-            help='do everything except store results into disk'
-        )
-        self._parser.add_option('-k', '--also-keywords',
-            action='store_true',
-            dest='include_keywords',
-            help='parse also suites\' and tests\' keywords'
-        )
-        self._parser.add_option('-f', '--files',
-            action='callback',
-            callback=files_args_parser,
-            dest='file_paths',
-            help='one or more Robot Framework output.xml files'
-        )
-
-    def _get_validated_options(self):
-        if len(argv) < 2:
-            self._exit_with_help()
-        options = super(ReaderOptions, self)._get_validated_options()
-        if options.file_paths is None or len(options.file_paths) < 1:
-            self._parser.error('at least one input file is required')
-        for file_path in options.file_paths:
-            if not exists(file_path):
-                self._parser.error('file %s not exists' % file_path)
-        return options
