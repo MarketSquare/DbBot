a0a39210fb04127e8c8a70bb46fbf364808cc676
Tatu Kairi <tatu.kairi@eficode.com>
2014-03-31 13:03:05 +0300

    Release 0.1

--- dbbot/__init__.py
+++ dbbot/__init__.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = '0.1-devel'
+__version__ = '0.1'
 
 from .logger import Logger
 from .robot_database import RobotDatabase
