f2bb61cfa8c5b66498df3c606c0fec9ce6b43dbd
Tattoo <tatu.kairi@eficode.com>
2014-05-15 17:45:39 +0300

    fix failbot example to work

--- examples/failbot/failbot/writer_options.py
+++ examples/failbot/failbot/writer_options.py
@@ -1,28 +1,27 @@
 from os.path import exists
 from sys import argv
 
-from dbbot import CommandLineOptions
+from dbbot.reader.reader_options import ReaderOptions
 
 
-class WriterOptions(CommandLineOptions):
+class WriterOptions(ReaderOptions):
 
     @property
     def output_file_path(self):
-        return self._options.output_file_path
-
-    def _add_parser_options(self):
-        super(WriterOptions, self)._add_parser_options()
-        self._parser.add_option('-o', '--output',
-            dest='output_file_path',
-            help='path to the resulting html file',
-        )
+        return self._target_file
 
     def _get_validated_options(self):
+        self._parser.set_usage('%prog [options] outfile')
         if len(argv) < 2:
             self._exit_with_help()
-        options = super(WriterOptions, self)._get_validated_options()
-        if not options.output_file_path:
-            self._parser.error('output html filename is required')
+        options, target_files = super(WriterOptions, self)._get_validated_options()
+        self._target_file = target_files.pop()
         if not exists(options.db_file_path):
-            self._parser.error('database %s not exists' % options.db_file_path)
-        return options
+            self._parser.error('database "%s" does not exists' % options.db_file_path)
+        return options, self._target_file
+
+    def _check_files(self, files):
+        if not files:
+            self._parser.error('output file not given')
+        for path in files:
+            open(path, 'a').close()
